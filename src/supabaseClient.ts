1e6dac531a7251ceb919c048e3fb9e40e40eff2f
everafterinvites <contact.everafterinvites@gmail.com>
2026-06-15 01:25:09 +0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // These values are public (they are meant to be visible)
 const supabaseUrl = "https://pvuszjcuvkycprbggweo.supabase.co/rest/v1/";
-const supabaseAnonKey = "sb_secret_SipsZKDRny3wJbMfI1m-5w_rN9Blc9l";
+const supabaseAnonKey = "sb_publishable_q7mtNLl06q5E_4huRpiOpw_t6Vd7ezK";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
